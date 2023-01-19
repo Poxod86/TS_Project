12ff4cace57d796e1d24de75cf42088be86e3826
Sergeevich <mrssn84@mail.ru>
2023-01-19 23:52:04 +0400

    lesson1

---
+++ lesson1/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Number
+// let a = 1;
+// let b = -10;
+// let c = 3.5;
+// let d = 10_000;
+// Сложение
+let revenue = 1000;
+let bonus = 500;
+let res = revenue + bonus;
+console.log(res);
---
+++ lesson1/app.ts
@@ -0,0 +1,13 @@
+// Number
+
+// let a = 1;
+// let b = -10;
+// let c = 3.5;
+// let d = 10_000;
+
+// Сложение
+let revenue: number = 1000;
+let bonus: number = 500;
+
+let res: number = revenue + bonus;
+console.log(res);
