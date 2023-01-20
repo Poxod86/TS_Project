1ec592e00ffbf4082f13130b334329940ec8f501
Sergeevich <mrssn84@mail.ru>
2023-01-20 15:30:16 +0400

    Часть 1 Основные типы

--- lesson1/app.js
+++ lesson1/app.js
@@ -9,3 +9,65 @@ let revenue = 1000;
 let bonus = 500;
 let res = revenue + bonus;
 console.log(res);
+// Типы в функциях
+function getFullName(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+// для стрелочной функции
+const getFullNameArrow = (firstname, surname) => {
+    return `${firstname} ${surname}`;
+};
+console.log(getFullName('Сергей', 'Сергеевич'));
+// Объекты
+function getFullNameObj(userEntitty) {
+    return `${userEntitty.firstname} ${userEntitty.surname}`;
+}
+const user = {
+    firstname: 'Sergey',
+    surname: 'Sergeevich',
+    city: 'Saratov',
+    age: 39,
+    skills: {
+        dev: true,
+        soft: false
+    }
+};
+console.log(getFullNameObj(user));
+// Массивы
+const skills = ['Dev', 'DevOps'];
+for (const skill of skills) {
+    console.log(skill.toLowerCase());
+}
+// Tuples (Кортежи)
+const sk = [1, 'Dev'];
+const idSk = sk[0];
+const nameSk = sk[1];
+//или
+const [id, skillname] = sk; //Деструктаризация
+// использование спредоператоров на типах
+const arr = [1, 'Dev', true, true, false];
+for (const item of arr) {
+    console.log(item);
+}
+;
+// Readonly
+// предотвращает изменять массив (только чтение)
+const array = [1, 'aaa'];
+const arrays = ['a', 'b']; // Альтернативная запись 
+// Enums
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FALLED"] = 3] = "FALLED";
+})(StatusCode || (StatusCode = {}));
+const result = {
+    messege: 'Платеж успешен',
+    statusCode: StatusCode.SUCCESS
+};
+// 1- успех
+// 2- в процессе
+// 3 - отклонен
+if (result.statusCode === StatusCode.SUCCESS) {
+    console.log(result.messege);
+}
--- lesson1/app.ts
+++ lesson1/app.ts
@@ -11,3 +11,84 @@ let bonus: number = 500;
 
 let res: number = revenue + bonus;
 console.log(res);
+
+// Типы в функциях
+
+function getFullName(firstname:string , surname:string): string {
+	return `${firstname} ${surname}`;
+}
+// для стрелочной функции
+const getFullNameArrow = (firstname:string , surname:string): string => {
+	return `${firstname} ${surname}`;
+}
+
+console.log(getFullName('Сергей', 'Сергеевич'));
+
+// Объекты
+
+function getFullNameObj(userEntitty: {firstname:string, surname:string}): string {
+	return `${userEntitty.firstname} ${userEntitty.surname}`;
+}
+const user = {
+	firstname: 'Sergey',
+	surname: 'Sergeevich',
+	city: 'Saratov',
+	age: 39,
+	skills: {
+		dev: true,
+		soft: false
+	}
+};
+
+console.log(getFullNameObj(user));
+
+// Массивы
+ const skills: string[] = ['Dev', 'DevOps'];
+
+ for (const skill of skills) {
+	console.log(skill.toLowerCase());
+ }
+
+ // Tuples (Кортежи)
+
+ const sk: [number, string] = [1, 'Dev'];
+ const idSk = sk[0];
+ const nameSk = sk[1];
+ //или
+ const [id, skillname] = sk; //Деструктаризация
+
+// использование спредоператоров на типах
+const arr: [number, string, ...boolean[]] = [1, 'Dev', true, true, false];
+for (const item of arr) {
+	console.log(item);
+};
+
+// Readonly
+// предотвращает изменять массив (только чтение)
+
+const array: readonly [number, string] = [1, 'aaa'];
+const arrays: ReadonlyArray<string> = ['a','b']; // Альтернативная запись 
+
+// Enums
+
+enum StatusCode {
+	SUCCESS = 1,
+	IN_PROCESS,
+	FALLED
+}
+
+const result = {
+	messege: 'Платеж успешен',
+	statusCode: StatusCode.SUCCESS
+};
+
+// 1- успех
+// 2- в процессе
+// 3 - отклонен
+
+if (result.statusCode === StatusCode.SUCCESS) {
+	console.log(result.messege)
+}
+
+
+
